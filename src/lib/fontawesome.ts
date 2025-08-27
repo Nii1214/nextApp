8810bf1d641253443ddc3f9dcf4c5217b3bad99a
Nii1214 <nii.tomoyuki1214@gmail.com>
2025-08-27 17:26:12 +0900

    feat:ラップトップアイコンを追加

--- src/lib/fontawesome.ts
+++ src/lib/fontawesome.ts
@@ -1,36 +1,7 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-    faUser,
-    faSearch,
-    faEdit,
-    faTrash,
-    faPlus,
-    faCheck,
-    faTimes,
-    faEye,
-    faEyeSlash,
-    faCog,
-    faSignOutAlt,
-    faHome,
-    faList,
-    faChartBar,
-    faUsers,
-    faUserShield,
-    faUserCheck,
-    faUserTimes
-} from '@fortawesome/free-solid-svg-icons';
-import {
-    faUser as faUserRegular,
-    faEdit as faEditRegular,
-    faTrashCan as faTrashCanRegular
-} from '@fortawesome/free-regular-svg-icons';
+import { faLaptopCode } from '@fortawesome/free-solid-svg-icons';
 
 // アイコンライブラリにアイコンを追加
-library.add(
-    // Solid icons
-    faUser, faSearch, faEdit, faTrash, faPlus, faCheck, faTimes,
-    faEye, faEyeSlash, faCog, faSignOutAlt, faHome, faList,
-    faChartBar, faUsers, faUserShield, faUserCheck, faUserTimes,
-    // Regular icons
-    faUserRegular, faEditRegular, faTrashCanRegular
-);
+library.add(faLaptopCode);
+
+export default library;
